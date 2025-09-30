2d63c38c2b44cb5f164480a69cb0f2cc2435dd9f
Román <geronimotomascampos@gmail.com>
2025-09-29 21:56:31 -0300

    otras modificaciones

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -4,9 +4,9 @@ import TextType from './TextType.jsx'
 
 const Inicio = () => {
   return (
-    <div className='bg-black/50 h-screen w-full text-center p-50 '>
+    <div className='bg-black/50 h-screen w-full text-center items-center'>
       <TextType 
-      className='text-white text-5xl font-bold text-center w-full '
+      className='text-white text-5xl mt-5 font-bold text-center w-full h-full'
         text={["Dia del Turismo", "Bienvenidos a Esquel", "Feliz Dia del Turismo!"]}
       typingSpeed={75}
       pauseDuration={1500}
